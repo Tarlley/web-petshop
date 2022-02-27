d9c8b4c23fef4c16342461cf9ecc4f4454c77b31
Wendel Tarlley <wendeltarlley@gmail.com>
2022-02-27 12:09:33 -0300

    Iniciando a aplicação com a tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from "@angular/material/icon";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HomeComponent } from './view/home/home.component'
+import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatCheckboxModule 
+  ],
+  exports:[
   ],
   providers: [],
   bootstrap: [AppComponent]
